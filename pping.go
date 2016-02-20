c7016dbf5688b6c1baa71aab82bf41d3f12c7bad
Jeffrey Forman <jeff@jeffreyforman.net>
2016-02-20 15:51:26 -0500

    cleaned up a bit of ping/pingdata structs. integrated creating []string of metrics.
    NEXT: actually sending the metrics to carbon/graphite

--- pping.go
+++ pping.go
@@ -19,27 +19,32 @@ import (
 )
 
 var (
-	hostsFlag     string // Flag hosts: Comma-seperated list of IP/hostnames to ping
-	pingCountFlag uint64 // Flag count: Uint8 Interger number of pings to send per cycle.
-	oneshotFlag   bool
-	intervalFlag  time.Duration
+	carbonHostPortFlag string // Flag carbon: Host:port of Carbon Cache line receiver
+	hostsFlag          string // Flag hosts: Comma-seperated list of IP/hostnames to ping
+	pingCountFlag      uint64 // Flag count: Uint8 Interger number of pings to send per cycle.
+	oneshotFlag        bool
+	intervalFlag       time.Duration
 
 	re_ping_packetloss *regexp.Regexp
 	re_ping_rtt        *regexp.Regexp
 	re_ping_hostname   *regexp.Regexp
 
-	pingBinary string // Path to ping binary based upon operating system
+	pingBinary string // Path to ping binary based upon operating system)
 )
 
-type PingData struct {
-	localHost       string
-	destinationHost string
-	time            int64
-	loss            float64
-	min             float64
-	avg             float64
-	max             float64
-	mdev            float64
+type PingStats struct {
+	loss float64
+	min  float64
+	avg  float64
+	max  float64
+	mdev float64
+}
+
+type Ping struct {
+	origin      string
+	destination string
+	time        int64
+	stats       PingStats
 }
 
 func setOsParams() {
@@ -85,30 +90,31 @@ func getValidHosts(hosts []string) []string {
 }
 
 // https://github.com/StefanSchroeder/Golang-Regex-Tutorial/blob/master/01-chapter2.markdown
-func parsePingOutput(pingOutput string, pingErr bool) PingData {
-	var data PingData
+func makePing(pingOutput string, pingErr bool) Ping {
+	var ping Ping
+	var stats PingStats
 	now := time.Now()
-	data.time = now.Unix()
-	localHost, _ := os.Hostname()
-	data.localHost = localHost
+	ping.time = now.Unix()
+	origin, _ := os.Hostname()
+	ping.origin = origin
 
 	re_ping_hostname_matches := re_ping_hostname.FindAllStringSubmatch(pingOutput, -1)[0]
-	data.destinationHost = re_ping_hostname_matches[1]
+	ping.destination = re_ping_hostname_matches[1]
 
 	re_packetloss_matches := re_ping_packetloss.FindAllStringSubmatch(pingOutput, -1)[0]
-	data.loss, _ = strconv.ParseFloat(re_packetloss_matches[1], 64)
+	stats.loss, _ = strconv.ParseFloat(re_packetloss_matches[1], 64)
 
 	if pingErr == true {
-		data.min, data.avg, data.max, data.mdev = 0, 0, 0, 0
+		stats.min, stats.avg, stats.max, stats.mdev = 0, 0, 0, 0
 	} else {
 		re_rtt_matches := re_ping_rtt.FindAllStringSubmatch(pingOutput, -1)[0]
-		data.min, _ = strconv.ParseFloat(re_rtt_matches[1], 64)
-		data.avg, _ = strconv.ParseFloat(re_rtt_matches[2], 64)
-		data.max, _ = strconv.ParseFloat(re_rtt_matches[3], 64)
-		data.mdev, _ = strconv.ParseFloat(re_rtt_matches[4], 64)
+		stats.min, _ = strconv.ParseFloat(re_rtt_matches[1], 64)
+		stats.avg, _ = strconv.ParseFloat(re_rtt_matches[2], 64)
+		stats.max, _ = strconv.ParseFloat(re_rtt_matches[3], 64)
+		stats.mdev, _ = strconv.ParseFloat(re_rtt_matches[4], 64)
 	}
-
-	return data
+	ping.stats = stats
+	return ping
 }
 
 func executePing(host string, numPings uint64) (string, bool) {
@@ -123,11 +129,15 @@ func executePing(host string, numPings uint64) (string, bool) {
 	return s_out, pingError
 }
 
-func spawnPingLoop(c chan<- PingData, host string, numPings uint64, sleepTime time.Duration, oneshot bool) {
+func spawnPingLoop(c chan<- Ping,
+	host string,
+	numPings uint64,
+	sleepTime time.Duration,
+	oneshot bool) {
 	for {
 		raw_output, err := executePing(host, numPings)
-		data := parsePingOutput(raw_output, err)
-		c <- data
+		pingResult := makePing(raw_output, err)
+		c <- pingResult
 		time.Sleep(sleepTime)
 
 		if oneshot == true {
@@ -136,10 +146,28 @@ func spawnPingLoop(c chan<- PingData, host string, numPings uint64, sleepTime ti
 	}
 }
 
-func receivePingData(c <-chan PingData) {
+// TODO: figure out reflection here so i can just loop
+// over the struct fields
+func createCarbonMetrics(ping Ping) []string {
+	var out []string
+	formattedDestination := strings.Replace(ping.destination, ".", "_", -1)
+	prefix := fmt.Sprintf("ping.%v.%v", ping.origin,
+		formattedDestination)
+
+	out = append(out, (fmt.Sprintf("%s.loss %v %v", prefix, ping.stats.loss, ping.time)))
+	out = append(out, (fmt.Sprintf("%s.min %v %v", prefix, ping.stats.min, ping.time)))
+	out = append(out, (fmt.Sprintf("%s.max %v %v", prefix, ping.stats.max, ping.time)))
+	out = append(out, (fmt.Sprintf("%s.avg %v %v", prefix, ping.stats.avg, ping.time)))
+	out = append(out, (fmt.Sprintf("%s.mdev %v %v", prefix, ping.stats.mdev, ping.time)))
+	return out
+}
+
+func processPing(c <-chan Ping) {
 	for {
-		msg := <-c
-		fmt.Printf("data: %+v\n", msg)
+		pingResult := <-c
+		fmt.Printf("data: %+v\n", pingResult)
+		metrics := createCarbonMetrics(pingResult)
+		fmt.Printf("metrics: %v\n", metrics)
 	}
 }
 
@@ -150,10 +178,10 @@ func main() {
 	hosts := strings.Split(hostsFlag, ",")
 	validHosts := getValidHosts(hosts)
 	fmt.Printf("valid hosts: %v\n", validHosts)
-	var c chan PingData = make(chan PingData)
+	var c chan Ping = make(chan Ping)
 	for _, currentHost := range validHosts {
 		go spawnPingLoop(c, currentHost, pingCountFlag, intervalFlag, oneshotFlag)
 	}
-	receivePingData(c)
+	processPing(c)
 	fmt.Println("end of main")
 }
